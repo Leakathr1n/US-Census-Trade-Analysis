b1f94e5bac72e093b81cfe9926a2779fa3b4c147
Lea Kathrin Roeller <142701998+Leakathr1n@users.noreply.github.com>
2025-09-28 21:38:00 +0200

    Setting Up Folders & Importing data

---
+++ .Rprofile
@@ -0,0 +1,47 @@
+# Title: RProfile for Coding Sample to be submitted to Pre-Doc Position XXXXXX
+# Author: Lea Röller
+# Date: 28. September 2025
+# Last date modified: mentioned next to added code snippets
+
+
+## Folders  ----
+#Creating the folders that we need
+if (!dir.exists("00 Additional files")) dir.create("00 Additional files")
+if (!dir.exists("01 RDS")) dir.create("01 RDS")
+if (!dir.exists("02 Scripts")) dir.create("02 Scripts")
+if (!dir.exists("03 Outputs")) dir.create("03 Outputs")
+if (!dir.exists("Archive")) dir.create("Archive")
+
+
+
+
+
+## Libraries ----
+# Installing libraries that are not installed yet 
+
+
+# Loading libraries
+library(jsonlite) # PURPOSE
+library(glue) # PURPOSE
+library(writexl) # PURPOSE
+library(dplyr) # PURPOSE
+library(readxl) # PURPOSE
+
+
+
+
+## Functions ----
+
+# Useful (longer) functions
+
+
+
+
+
+## Graphic Options
+options(scipen = 999) # disable scientific notation for graphics later
+
+
+## Goodbye ----
+# Ensure that everything has run
+print ("Hello - let's go!")
---
+++ 02 Scripts/1. Importing Monthly US Commdities Imports.R
@@ -0,0 +1,44 @@
+########################################################################
+# Title: 1. Importing Monthly US Commdities Imports
+# Author: Lea Roeller
+# Date: 28/00/2025
+#######################################################################
+
+## 0. Housekeeping ----
+rm(list = ls()) #Clean everthing
+
+## 1. Importing data ----
+
+# More information on how to construct the URL / API can be found in the US Census guide linked the line below
+# US Census trade manual: https://www.census.gov/foreign-trade/reference/guides/Guide_to_International_Trade_Datasets.pdf
+# We are currently downloading US Commodities imports in $ with all available counterpart areas per month using HS2 codes
+
+month <- sprintf("%02d", 1:12) # generate the month variable
+year <- as.character(2023:2024) # generate the year variable
+
+full_imports <- list() # initialise a list for the data to be stored in
+
+for (y in year) {
+  for (m in month) {
+    print(paste("Processing", y, "-", m)) # Print the processing message
+    url <- paste0(
+      "https://api.census.gov/data/timeseries/intltrade/imports/hs?get=",
+      "I_COMMODITY,I_COMMODITY_LDESC,I_COMMODITY_SDESC,", # import variables
+      "CTY_CODE,CTY_NAME,GEN_VAL_MO", # levels
+      "&YEAR=", y, # year
+      "&MONTH=", m, # month
+      "&COMM_LVL=HS2" #HS2 codes
+    )
+    
+    json_data <- fromJSON(url) # data comes in JSON format
+    monthly_imports <- as.data.frame(json_data)  # convert to a dataframe
+    colnames(monthly_imports) <- monthly_imports[1, ] # set the first row as column names
+    monthly_imports <- monthly_imports[-1, ]  # remove the first row from data
+    full_imports[[length(full_imports) + 1]] <- monthly_imports
+  }
+}
+full_imports <-do.call(rbind,full_imports) #bind it all together
+
+## 2. Exporting data ----
+
+save(full_imports, file = "01 RDS/US Census Monthly.RData")
